ea044860740c3082a7569344cb683f8f8ea845a2
raun <rd31879@uga.edu>
2018-01-20 12:46:28 -0500

    extracted cats from y_train/test

--- preprocessing.py
+++ preprocessing.py
@@ -2,8 +2,6 @@ import numpy as np
 import urllib
 from pyspark import SparkContext, SparkConf
 import re
-
-#IN PROGRESS
 #we are using python 3 soooo dont need to specify utf8 :>
 #udf's 
 to_unicode(x):
@@ -23,7 +21,7 @@ fix_doc(x):
     x=re.sub("\.|,|:|;|\'|!|\?","",x)
     # next we will do maulik's stuff before making it lower 
     # uncomment to use also maybe verify my logic 
-    #x=re.sub("(\.[A-Z][a-zA-Z]*\s*)+","",x) 
+    #x=re.sub("[ ([A-Z][a-zA-Z]*\s*)+]","",x) 
     
     #next we change to lower case 
     #and remove quote and amp
@@ -32,6 +30,15 @@ fix_doc(x):
     #last step remove everything which is not a letter 
     x=re.sub('[^a-zA-Z]'," ",x)
     return x
+
+getcats(x):
+    fitered_data=[]
+    for i in x:
+        if ("CAT" or "cat") in i:
+            filtered_data.append(i)
+    return filtered_data
+        
+    
     
 
     
@@ -50,21 +57,21 @@ def main():
     
     #make local copy
     X_train = urllibURLopener()
-    X_train.retrieve(base_url_train+"/X_train_"+dataset_size+"X_to_train.txt")
+    X_train.retrieve(base_url_train+"/X_train_"+dataset_size,"X_to_train.txt")
     y_train = urllibURLopener()
-    y_train.retrieve(base_url_train+"/y_train_"+dataset_size+"y_to_train.txt")
+    y_train.retrieve(base_url_train+"/y_train_"+dataset_size,"y_to_train.txt")
         
     X_test = urllibURLopener()
-    X_test.retrieve(base_url_test+"/X_test_"+dataset_size+"X_to_test.txt")
+    X_test.retrieve(base_url_test+"/X_test_"+dataset_size,"X_to_test.txt")
     y_test = urllibURLopener()
-    y_test.retrieve(base_url_test+"/y_test_"+dataset_size+"y_to_test.txt")
+    y_test.retrieve(base_url_test+"/y_test_"+dataset_size,"y_to_test.txt")
     
     #we convert the text to utf-8 "the common unicode scheme" according to python docs
-    X_train=(sc.textFile("X_to_train.txt").lambda(to_unicode))
-    y_train=(sc.textFile("y_to_train.txt").lambda(to_unicode))
+    X_train=(sc.textFile("X_to_train.txt").map(to_unicode))
+    y_train=(sc.textFile("y_to_train.txt").map(to_unicode))
         
-    X_test=(sc.textFile("X_to_test.txt").lambda(to_unicode))
-    y_test=(sc.textFile("y_to_test.txt").lambda(to_unicode))
+    X_test=(sc.textFile("X_to_test.txt").map(to_unicode))
+    y_test=(sc.textFile("y_to_test.txt").map(to_unicode))
     
     #we are going to do what dr quinn mentioned and not use my crazy key gens
     #fix_doc is going to be the main cleanup function
@@ -73,11 +80,18 @@ def main():
     X_train=X_train.zipWithIndex().map(lambda x:(x[1],fix_doc(x[0])))
     X_test=X_test.zipWithIndex().map(lambda x:(x[1],fix_doc(x[0])))
     
+    #mapping X_train to y_train and X_test to y_test
+    
+    
+    y_train=ytrain.map(lambda x:x.split(",")).map(getcats).lower()
+    
+    
     
 
     
     
     
+ 
     
     
     
@@ -85,43 +99,4 @@ def main():
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-        
-        
-
-
     
