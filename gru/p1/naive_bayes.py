3a07f9d07c11780d8f048c16f687744f3a446a57
raun1 <raunak.dey25@uga.edu>
2018-02-02 12:00:59 -0500

    updated comments on naive_bayes.py
    Gives 92% accuracy

--- gru/p1/naive_bayes.py
+++ gru/p1/naive_bayes.py
@@ -11,18 +11,14 @@ sc = SparkContext.getOrCreate()
 import re
 from collections import Counter
 from string import punctuation
-from nltk.stem import PorterStemmer
-ps = PorterStemmer()
-#To run this code type python3 preprocessing.py <train_url_location> <test_url_location> <dataset_size> <0-you have y_test else 1 if you dont>
+
+
+#To run this code type python3 naive_bayes.py <train_url_location> <test_url_location> <dataset_size> <1>
 #The following code preprocess the text and lables set for the large/small/vsmall sets. 
 #It stores it under X_train_clean.txt, y_train_clean.txt, X_train_clean.txt and y_test_clean.txt (Optional)
 #in the same folder where it is called. It will automatically collect the data and create these four files
-#To delete the raw files please uncomment line number 189 and 190
-#If you have y_test i.e. the test lables please enter 0 in command line as the fourth parameter
+
 ########################################################################################################################
-"""Thia function does the basic preprocessing of the data.It remove punctuations,special characters and extra white spaces. All the words are 
-   converted to lowercase.The text also contains characters like &quot and &amp which have been removed.
-"""
 def fix_doc(x):
     #basic preprocessing - removing the same stuff from project 0 but later we will add more 
     #example we can import punctuations from string and use it but later :>
@@ -33,6 +29,9 @@ def fix_doc(x):
 
     #x=x.strip(".,:;'!?")
     
+    # next we will do maulik's stuff before making it lower 
+    # uncomment to use also maybe verify my logic 
+    #x=re.sub("[ ([A-Z][a-zA-Z]*\s*)+]","",x) 
     
     #next we change to lower case 
     #and remove quote and amp
@@ -44,19 +43,15 @@ def fix_doc(x):
     a=""
     x=x.split(" ")
     for i in range(0,len(x)):
-        if(x[i] not in stopWords.value):
+        if(x[i] not in stopWords.value and (len(x[i])>2)):
             a=a+x[i]+" "
             
     a=a.strip()
     return a
 #######################################################################################################################
-"""
-This function scans the preprocessed files and returns data containing all the four classes or all the adta containing CAT
-which is the suffix of the four classes.
-"""
-
 def getcats(x):
-    #scans and extract stuff with CAT/cat in suffix and makes it lower case
+    #This function scans the preprocessed files and returns data containing all the four classes or all the adta containing CAT
+    #which is the suffix of the four classes.
     x=str(x, "utf-8")
     x=x.split(",")
     fitered_data=[]
@@ -66,14 +61,11 @@ def getcats(x):
     return fitered_data
         
 ######################################################################################################################  
-"""
-Some words in the text belong to two different classes.In such a case, the data is saved twice as belonging to a different 
-in each case.
-"""
-
 def split_data(x):
     #splits the key value pair into multiple rows if the value has multiple values eg,
     #input [0,(first,second)] --> [[0,first],[0,second]]
+    #Some words in the text belong to two different classes.In such a case, the data is saved twice as belonging to a different 
+    #in each case.
     combined=[]
     
     for j in range(0,len(x)):
@@ -90,12 +82,12 @@ def split_data(x):
 ######################################################################################################################
 
 ######################################################################################################################
-"""
-This function calculates the number of terms belonging to each of the four classes and returns the count for all the
-classes.
-"""
+#def create_array(x):
+#    ccat=
+######################################################################################################################
 
 def create_array1(x,a,b,c,d):
+    #calculates the log of the word frequencies
     
     ccat=1
     ecat=1
@@ -114,31 +106,18 @@ def create_array1(x,a,b,c,d):
             gcat=gcat+(float)(x[k+1])
 
 
-    #if(ccat>0):
+    
     ccat=math.log(ccat/(a+vocab.value))
-    #if(ecat>0):
+    
     ecat=math.log(ecat/(c+vocab.value))
-    #if(mcat>0):
+   
     mcat=math.log(mcat/(b+vocab.value))
-    #if(gcat>0):
+    
     gcat=math.log(gcat/(d+vocab.value))
-    '''
-    if(ccat>0):
-        ccat=math.log(ccat/a)
-    if(ecat>0):
-        ecat=math.log(ecat/c)
-    if(mcat>0):
-        mcat=math.log(mcat/b)
-    if(gcat>0):
-        gcat=math.log(gcat/d)
-    '''
+    
     return [ccat,mcat,ecat,gcat]
-###################################################################################################################
-"""
-"""
-
-
 def predict_class(x,a,b,c,d):
+    #returns a prediction for the X_test tokens by using naive-bayes model
     class_values=group_by_class.value    
 
     
@@ -188,14 +167,12 @@ def predict_class(x,a,b,c,d):
     else:
         return "GCAT"
         #return str(product[3])
-###################################################################################################################
-"""
-This function reads the Stop Words from the file containing stopwords. "stopWords.txt" 
-It uses a subset of the words given from the nltk python library.
 
-"""
+
             
-            
+#####################################################################################
+    #This function reads the Stop Words from the file containing stopwords. "stopWords.txt" 
+              
 
 def read_stop_words():
     with open("stopWordList.txt") as f:
@@ -209,7 +186,7 @@ if __name__ == "__main__":
 
     
     if len(sys.argv) != 5:
-        print("Usage: project_1.py <url_train> <url_test> <vsmall/small/large> <flag=0 if y_test is present 1 if not", file=sys.stderr)
+        print("Usage: project_1.py <url_train> <url_test> <vsmall/small/large> <1>", file=sys.stderr)
         exit(-1)
     spark = SparkSession\
         .builder\
@@ -250,33 +227,18 @@ if __name__ == "__main__":
     
     y_train=(y_train.map(getcats)\
         .zipWithIndex().map(lambda x:(x[1],x[0]))).collect()
+
     #splitting the X_train and y train's with multiple copies
     y_train=sc.parallelize(split_data(y_train))
     
     
     train_complete=X_train.join(y_train).map(lambda x:(x[1][0],x[1][1]))    
     
-    print("check1")
+    #seperates the X_train and y_train
     X_train=train_complete.map(lambda x:x[0])
     
     y_train=train_complete.map(lambda x:x[1])
-   
-    print("check1.25")
-    if(flag==0):
-       
-        y_test=(sc.textFile("y_to_test.txt").map(lambda x:x.encode("utf-8")))
-    
-        x=(str("wget "+base_url_test+"y_test_"+dataset_size+" -O y_to_test.txt"))
-        os.system(x)
-        y_test=(y_test.map(getcats)\
-            .zipWithIndex().map(lambda x:(x[1],x[0]))).collect()
-        y_test=sc.parallelize(split_data(y_test))
-        test_complete=X_test.join(y_test).map(lambda x:(x[1][0],x[1][1]))
-        X_test=test_complete.map(lambda x:x[0])
-        y_test=test_complete.map(lambda x:x[1])
-       
-
-    
+    #calculates the total number of documents in different document class
     temp=y_train.map(lambda x:(x,1)).reduceByKey(lambda a,b:a+b).collect()
     print (temp)
     tot_ccat=temp[0][1]
@@ -287,21 +249,21 @@ if __name__ == "__main__":
     
 
     
-    
+    #calculates the probability of a document belonging to a single class
     prior_ccat=(math.log(((float)(tot_ccat))/((float)(tot_ccat+tot_gcat+tot_mcat+tot_ecat))))
     prior_gcat=(math.log(((float)(tot_gcat))/((float)(tot_ccat+tot_gcat+tot_mcat+tot_ecat))))
     prior_ecat=(math.log(((float)(tot_ecat))/((float)(tot_ccat+tot_gcat+tot_mcat+tot_ecat))))
     prior_mcat=(math.log(((float)(tot_mcat))/((float)(tot_ccat+tot_gcat+tot_mcat+tot_ecat))))
     #del(temp)
     
-    print("check2")
+    #generates the vocab by collecting all the unique words from all the documents of the training set
     vocab=(X_train.flatMap(lambda x:x.split(" ")).map(lambda x:re.sub(",","", x)).distinct().map(lambda x:(1,1)).reduceByKey(lambda a,b:a+b).collect())[0][1]
     #print(vocab)
     vocab=sc.broadcast(vocab)
     
 
    
-    
+    #splits the words from X_train dataset into tuples like (hello ccat,1) and (hello gcat,1) and then reduces and finds individual word count perclass
     group_by_class=train_complete.map(lambda x:(x[1],x[0])).reduceByKey(lambda a,b:a+b).map(lambda x:([(x[0],z) for z in x[1].split(" ")]))\
                 .flatMap(lambda x:(x)).map(lambda x:(x,1)).reduceByKey(lambda a,b:a+b)
     
@@ -315,11 +277,11 @@ if __name__ == "__main__":
     tot_ecat=temp[2][1]
     tot_gcat=temp[3][1]
 
-    #build on this line
+    #converts the string doc matrix eg (hello, ccat 1 gcat 1) to hello,[1,1,0,0]
     group_by_class=group_by_class.map(lambda x:(x[0][1],(str(x[0][0])+" "+str(x[1])+" "))).reduceByKey(lambda a,b:str(a)+str(b))\
                 .map(lambda x:(x[0],str(x[1]).replace("cat","").split(" "))).map(lambda x:(x[0],create_array1(x[1],tot_ccat,tot_mcat,tot_ecat,tot_gcat))).collect()
 
-    #print((group_by_class.take(4)))
+    
 
     group_by_class=sc.broadcast(dict(group_by_class))
     
@@ -331,14 +293,12 @@ if __name__ == "__main__":
     
     
     
-    print("check3")
+   
     
-    #print(X_test.collect())
-    
-
+    #predicts on the testing set
     y_test=X_test.map(lambda x:Counter(x[1].split(" "))).map(lambda x:predict_class(x,prior_ccat,prior_mcat,prior_ecat,prior_gcat))
-    print("lastcheck")
     
+    #saveing to file
     submit=open('y_test.txt',"w")
     y_test=y_test.collect()
     counter=0
@@ -347,7 +307,6 @@ if __name__ == "__main__":
         #print(counter)
         submit.write("%s\n" % i)
     submit.close()
-    
     
     
 
